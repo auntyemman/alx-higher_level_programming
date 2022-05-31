d40f4b6d36b2807c2b82ea343725a781a2c53ae3
Adedayo Adepoju <1mole.coco@gmail.com>
2022-05-31 21:41:54 +0100

    combination of numbers with nested loops

---
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+for i in range(0, 9):
+    for j in range(1, 10):
+        if i > j or i == j:
+            continue
+        else:
+            if i == 8 and j == 9:
+                print("89")
+            else:
+                print("{}{},".format(i, j), end=' ')
