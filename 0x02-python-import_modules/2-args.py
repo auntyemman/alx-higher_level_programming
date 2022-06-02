a642669ebce410156cb8c5d157eed4ad6ac88744
Adedayo Adepoju <1mole.coco@gmail.com>
2022-06-02 19:11:20 +0100

    printing the list of arguments

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -4,14 +4,14 @@ from sys import argv
 
 if __name__ == "__main__":
 
-    total = len(argv) - 1
+    total = len(argv)
 
     if total <= 1:
         print("0 arguments.")
     else:
         if total == 2:
-            print("{:d} argument:".format(total))
+            print("{:d} argument:".format(total - 1))
         else:
-            print("{:d} arguments:".format(total))
-        for i in range(1, total + 1):
-            print("{:d}: {}".format(i, argv[i]))
+            print("{:d} arguments:".format(total - 1))
+        for i in range(1, total):
+            print("{:d}: {:s}".format(i, argv[i]))
