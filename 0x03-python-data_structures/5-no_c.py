0a52a7206d676bb1823be00f59c57bbbd8eed9b6
Adedayo Adepoju <1mole.coco@gmail.com>
2022-06-06 11:55:50 +0100

    a function tha removes letter 'c and 'C' from a string

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,9 +1,6 @@
 #!/usr/bin/python3
 
 def no_c(my_string):
-    """
-    A function that removes all characters c and C from a string
-    """
     new_list = list(my_string)
     new_string = ""
     for i in new_list:
