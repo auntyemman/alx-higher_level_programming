d02c3858cd94b0952b555949613c9e51c054b5f9
Adedayo Adepoju <1mole.coco@gmail.com>
2022-06-20 19:59:51 +0100

    A function that prints integers

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
+
 def safe_print_integer(value):
     """ A  function that prints an integer with"""
     try:
         print("{:d}".format(value))
-    except:
+    except Exception:
         return False
     else:
         return True
