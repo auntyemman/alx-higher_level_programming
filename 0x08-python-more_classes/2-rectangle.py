78a2f6b92545fb02b8e094c7ea50fb12213eb691
Adedayo Adepoju <1mole.coco@gmail.com>
2022-06-27 20:31:28 +0100

    Area and Perimeter

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -37,7 +37,6 @@ class Rectangle:
         elif value < 0:
             raise ValueError("height must be >= 0")
         self.__height = value
-    
 
     def area(self):
         return self.__height * self.__width
