fe012cee276c0befa2115bf736e075fe2fa63536
Adedayo Adepoju <1mole.coco@gmail.com>
2022-07-04 22:12:48 +0100

    Only sub class of

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -1,15 +1,9 @@
 #!/usr/bin/python3
-''' module: 4-inherits_from
-'''
+
+"""This module contains the function `inherits_from`"""
 
 
 def inherits_from(obj, a_class):
-    '''the object is an instance of a class that 
-    inherited (directly or indirectly)
-
-    obj: an object
-    a_class: a class
-    returns None
-    '''
-
-    return type(obj) != a_class and isinstance(obj, a_class)
+    """returns True if the object is an instance of a class\
+        that inherited from  a_class ; otherwise False"""
+    return (issubclass(type(obj), a_class) and type(obj) != a_class)
