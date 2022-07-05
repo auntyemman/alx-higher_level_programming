bc745874d1db861f138551bc9359c4154e4ce55e
Adedayo Adepoju <1mole.coco@gmail.com>
2022-07-05 21:55:57 +0100

    Load, add, save

---
+++ 0x0B-python-input_output/7-add_item.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+"""Module to load and save file"""
+import sys
+
+
+def main():
+    save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+    load_from_json_file = __import__(
+        '6-load_from_json_file').load_from_json_file
+
+    try:
+        items = load_from_json_file("add_item.json")
+    except FileNotFoundError:
+        items = []
+    items.extend(sys.argv[1:])
+    save_to_json_file(items, "add_item.json")
+
+
+main()
