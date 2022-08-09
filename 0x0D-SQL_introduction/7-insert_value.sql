12120e30893de3f129661f1071329c85fbbfa97b
Adedayo Adepoju <1mole.coco@gmail.com>
2022-08-09 22:07:27 +0100

    First row add

--- 0x0D-SQL_introduction/7-insert_value.sql
+++ 0x0D-SQL_introduction/7-insert_value.sql
@@ -1,5 +1,3 @@
--- Insert a new row in 'first_table' from db 'hbtn_0c_0'
--- id == 89
--- name == Holberton School
--- db name will be passed as arg to mysql cmd
-INSERT INTO `first_table` (`id`, `name`) VALUES (89, "Holberton School");
+-- Insert a row using INSERT
+-- DML query to inserts a new row into the table
+INSERT INTO first_table (id, name) VALUES (89, 'Holberton School');
