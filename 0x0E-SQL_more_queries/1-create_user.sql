f4e4caed62b0bdcb0225c0ddad2ba0fa98d81b69
Adedayo Adepoju <1mole.coco@gmail.com>
2022-08-10 21:42:17 +0100

    Initial commit

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,10 +1,7 @@
---This creates the MySQL server user 'user_0d_1' with priviledges.
-
-CREATE USER user_0d_1 IF NOT EXISTS
-IDENTIFIED BY user_0d_1_pwd;
-
-GRANT SELECT 
-
-ON *.* 
-
-TO user_0d_1@localhost;
+-- Creates the user user_0d_1 with all privileges.
+CREATE USER
+    IF NOT EXISTS 'user_0d_1'@'localhost'
+    IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES
+   ON *.*
+   TO 'user_0d_1'@'localhost';
