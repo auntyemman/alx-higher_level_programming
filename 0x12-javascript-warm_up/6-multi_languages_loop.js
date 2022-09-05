a06a121f7489b2c1d51998da1e4e9b4dd2b2a30d
Adedayo Adepoju <1mole.coco@gmail.com>
2022-09-05 21:46:38 +0100

    6. Loop to languages

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 let arr = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 
-for (let i = 0; i < arr.length; i++) {
+for (let i = 0, i < arr.length; i++) {
   console.log(arr[i]);
 }
