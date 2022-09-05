10aa0c8cc16e25f8c4b8e83f24d36fd63f23f858
Adedayo Adepoju <1mole.coco@gmail.com>
2022-09-05 22:25:31 +0100

    7. I love C

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -2,5 +2,5 @@
 if (isNaN(parseInt(process.argv[2]))) {
   console.log('Missing number of occurrences');
 } else {
-  console.log('C is fun\n'.repeat(parseInt(process.argv[2])));
+  console.log('C is fun\n'.repeat(parseInt(process.argv[2])).trimEnd());
 }
