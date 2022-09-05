e757d667818a1fee7d16df43603212963a5d6254
Adedayo Adepoju <1mole.coco@gmail.com>
2022-09-05 18:30:01 +0100

    4. Create a sentence

--- 0x12-javascript-warm_up/4-concat.js
+++ 0x12-javascript-warm_up/4-concat.js
@@ -1,8 +1,2 @@
 #!/usr/bin/node
-if (process.argv[3] !== null) {
-  console.log(process.argv[2] + ' is ' + process.argv[3]);
-} else if (process.argv[3] === null) {
-  console.log(process.argv[2] + ' is ' + process.argv[3]);
-} else if (process.argv[2] === null && process.arg[3] === null) {
-  console.log(process.argv[2] + ' is ' + process.argv[3]);
-}
+console.log(process.argv[2] + ' is ' + process.argv[3]);
