c6c89a459ca7e8bed5b1dc57944179702c801a91
Adedayo Adepoju <1mole.coco@gmail.com>
2022-09-06 21:48:45 +0100

    Square #1

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,12 +1,21 @@
 #!/usr/bin/node
-const square = require('./5-square');
-class Square extends square {
+const Square_ = require('./5-square');
+
+const Square = class Square extends Square_ {
   charPrint (c) {
-    if (!c) {
-      c = 'X';
+    if (c) {
+      let prints = '';
+      for (let cont = 0; cont < this.height; cont++) {
+        for (let cont = 0; cont < this.height; cont++) {
+          prints = prints + c;
+        }
+        console.log(prints);
+        prints = '';
+      }
+    } else {
+      super.print();
     }
-    this.print(c);
   }
-}
+};
 
 module.exports = Square;
