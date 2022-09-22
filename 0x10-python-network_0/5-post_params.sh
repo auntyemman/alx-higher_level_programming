92b6b8c976be25709f449aab2a8a2b7d2052be0a
Adedayo Adepoju <1mole.coco@gmail.com>
2022-09-22 22:32:28 +0100

    cURL POST parameters

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Script sends POST to URL
-curl -sX POST -d "email=test@gmail.com&subject=I will always be here for PLD" "$
+curl -sL "$1" -X POST -d "email=hr@holbertonschool.com&subject=I will always be here for PLD"
