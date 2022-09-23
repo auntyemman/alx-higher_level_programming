7ae0af141497435cd5781a9d6b8d5fb20c8ed354
Adedayo Adepoju <1mole.coco@gmail.com>
2022-09-23 21:58:01 +0100

    Initial commit

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,14 +1,10 @@
 #!/usr/bin/python3
-"""A script that
-fetches https://intranet.hbtn.io/status.
-"""
+"""fetches https://intranet.hbtn.io/status"""
+from urllib import request
 
-
-if __name__ == '__main__':
-    import urllib.request
-
-    with urllib.request.urlopen('https://intranet.hbtn.io/status') as resp:
-        content = resp.read()
+if __name__ == "__main__":
+    with request.urlopen('https://intranet.hbtn.io/status') as res:
+        content = res.read()
         print("Body response:")
         print("\t- type: {}".format(type(content)))
         print("\t- content: {}".format(content))
