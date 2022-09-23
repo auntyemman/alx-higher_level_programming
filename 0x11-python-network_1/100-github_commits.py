9217707c539ecfeeeaf0d8fdd7a7e44457d15677
Adedayo Adepoju <1mole.coco@gmail.com>
2022-09-23 22:56:07 +0100

    Initial commit

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -1,20 +1,14 @@
 #!/usr/bin/python3
-"""Lists the 10 most recent commits on a given GitHub repository.
-Arguments: arg1: repo name, arg2: repo owner
 """
-import sys
+Python script that takes 2 arguments in order to solve the given challenge
+"""
 import requests
+from sys import argv
 
-
-if __name__ == "__main__":
-    # Get the arguments, arg1: repo name, arg2: repo owner
-    url = "https://api.github.com/repos/{}/{}/commits".format(
-            sys.argv[2], sys.argv[1])
-
+if __name__ == '__main__':
+    url = "https://api.github.com/repos/{}/{}/commits".format(argv[2], argv[1])
     r = requests.get(url)
     commits = r.json()
-    try:
-        for i in range(10):
-            print(f"{commits[i]['sha']}: {commits[i]['commit']['author']['name']}")
-    except IndexError:
-        pass
+    for commit in commits[:10]:
+        print(commit.get('sha'), end=': ')
+        print(commit.get('commit').get('author').get('name'))
