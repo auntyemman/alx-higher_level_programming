e72e11b24388bdb13244866b848cf5581d27e21e
Adedayo Adepoju <1mole.coco@gmail.com>
2022-09-23 23:04:23 +0100

    Initial commit

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,13 +1,12 @@
 #!/usr/bin/python3
-"""
-Script that fetches https://intranet.hbtn.io/status
-"""
-import requests
+"""fetches https://alx-intranet.hbtn.io/status"""
+from requests import get
+
 
 if __name__ == '__main__':
-    url = "https://intranet.hbtn.io/status"
-    r = requests.get(url)
-    text = r.text
-    print("Body response:")
-    print("\t- type: {}".format(type(text)))
-    print("\t- content: {}".format(text))
+    url = "https://alx-intranet.hbtn.io/status"
+
+    response = get(url)
+    b_content = response.text
+    string = "Body response:\n\t- type: {}\n\t- content: {}"\
+        .format(type(b_content), b_content)
