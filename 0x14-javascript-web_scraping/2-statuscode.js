ecc1ca90c8e10b3018982c0053cf7ce0b71fa081
Adedayo Adepoju <1mole.coco@gmail.com>
2022-10-18 22:47:28 +0100

    initial commit

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -1,10 +1,10 @@
 #!/usr/bin/node
-// A script that display the status code of a GET request
 
-const args = process.argv;
-let request = require('request');
-request(args[2], function (error, response, body) {
-  if (error) {
-    console.log('error:', error); // Print the error if one occurred
-  } else console.log('code:', response && response.statusCode);
+const url = process.argv[2]
+const request = require('request')
+
+request.get(url, function (error, response, body) {
+    if (!error) {
+        console.log('code:', response.statusCode)
+    }
 });
